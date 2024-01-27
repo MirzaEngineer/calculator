375e1f99995c1f78eface94fed162f31fe0406a5
MirzaEngineer <um2876269@gmail.com>
2024-01-27 01:18:35 -0800

    remove extra code

--- index.ts
+++ index.ts
@@ -1,31 +1,3 @@
-// import chalk from "chalk";
-// import inquirer from "inquirer";
-// console.log(chalk.red.bgGreen.bgYellow("Mirza Usama"));
-
-// const data = inquirer.prompt([
-//     {
-//         name: "num1",
-//         type: "number",
-//         message: "Enter number1",
-//     }, {
-//         name: "num2",
-//         type: "number",
-//         message: "Enter number2?"
-//     }, {
-//         name: "operate",
-//         type: "list",
-//         message: "Operation",
-//         choices: ["+", "*", "/", "-", "^",]
-//     }
-    
-// ]).then((value)=>{
-//     console.log(value);
-    
-
-// })
-// console.log(data);
-
-
 import inquirer, { Answers, QuestionCollection } from "inquirer"
 import chalk from "chalk"
 
