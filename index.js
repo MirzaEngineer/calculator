ea3cb59c0f3f00fa996ee287c175fc613d9f02a4
MirzaEngineer <um2876269@gmail.com>
2024-01-27 01:15:56 -0800

    added code

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // import chalk from "chalk";
 // import inquirer from "inquirer";
 // console.log(chalk.red.bgGreen.bgYellow("Mirza Usama"));
-
 // const data = inquirer.prompt([
 //     {
 //         name: "num1",
@@ -17,60 +16,50 @@
 //         message: "Operation",
 //         choices: ["+", "*", "/", "-", "^",]
 //     }
-    
 // ]).then((value)=>{
 //     console.log(value);
-    
-
 // })
 // console.log(data);
-
-
-import inquirer, { Answers, QuestionCollection } from "inquirer"
-import chalk from "chalk"
-
-const questions: QuestionCollection = [{
-    name: "num1",
-    type: "number",
-    message: "Enter number1",
-}, {
-    name: "num2",
-    type: "number",
-    message: "Enter number2?"
-}, {
-    name: "operate",
-    type: "list",
-    message: "Operation",
-    choices: ["+", "*", "/", "-", "^",]
-}
-]
-var reply: Promise<Answers> = inquirer.prompt(questions)
-
-reply.then((reply: Answers) => {
+import inquirer from "inquirer";
+import chalk from "chalk";
+const questions = [{
+        name: "num1",
+        type: "number",
+        message: "Enter number1",
+    }, {
+        name: "num2",
+        type: "number",
+        message: "Enter number2?"
+    }, {
+        name: "operate",
+        type: "list",
+        message: "Operation",
+        choices: ["+", "*", "/", "-", "^",]
+    }
+];
+var reply = inquirer.prompt(questions);
+reply.then((reply) => {
     switch (reply.operate) {
         case "+":
             console.log(chalk.red.bgWhite(`${reply.num1} + ${reply.num2} = ${reply.num1 + reply.num2}`));
             break;
         case "*":
-            console.log (chalk.blue.bgYellow(`${reply.num1} * ${reply.num2} = ${reply.num1 * reply.num2}`));
+            console.log(chalk.blue.bgYellow(`${reply.num1} * ${reply.num2} = ${reply.num1 * reply.num2}`));
             break;
         case "-":
             console.log(chalk.black.bgGreen(`${reply.num1} - ${reply.num2} = ${reply.num1 - reply.num2}`));
             break;
         case "/":
-            console.log (chalk.yellow.bgGray (`${reply.num1} / ${reply.num2} = ${reply.num1 / reply.num2}`));
+            console.log(chalk.yellow.bgGray(`${reply.num1} / ${reply.num2} = ${reply.num1 / reply.num2}`));
             break;
         case "^":
-            console.log(chalk.cyanBright.bgRedBright (`${reply.num1} ^ 2 = ${reply.num1 * reply.num2}`));
+            console.log(chalk.cyanBright.bgRedBright(`${reply.num1} ^ 2 = ${reply.num1 * reply.num2}`));
             break;
         default:
             break;
     }
-// console.log("replies", reply);
-
-})
+    // console.log("replies", reply);
+});
 reply.catch((error) => {
     console.log("Error", error);
-
-    
 });
